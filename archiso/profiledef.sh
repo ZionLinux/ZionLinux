4ac2b42ae852e46838036a487b20c4ee3d7efecb
Andrew <76942911+alexfeed1990@users.noreply.github.com>
2021-11-20 14:23:36 +0200

    dumass

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -20,8 +20,8 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["usr/bin/calamares"]="0:0:755"
-  ["usr/bin/calamares_polkit"]="0:0:755"
+  ["/usr/bin/calamares"]="0:0:755"
+  ["/usr/bin/calamares_polkit"]="0:0:755"
   ["/etc/gshadow"]="0:0:0400"
   ["/etc/sudoers.d"]="0:0:750"
 )
