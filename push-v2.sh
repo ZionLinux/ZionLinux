c667d603bc57d28e6576b1bc60dec022134645bc
alexfeed1990 <alexfeed1990real@gmail.com>
2022-02-07 12:58:25 +0200

    aaa

--- push-v2.sh
+++ push-v2.sh
@@ -1,27 +1,16 @@
+#!/bin/bash
 
-# checking if I have the latest files from github
-echo "Checking for newer files online first"
 git pull
 
-# Below command will backup everything inside the project folder
 git add --all .
 
-# Give a comment to the commit if you want
-echo "####################################"
-echo "Write your commit comment!"
-echo "####################################"
-
+echo "Please write your commit message."
 read input
-
-# Committing to the local repository with a message containing the time details and commit text
 
 git commit -m "$input"
 
-# Push the local files to github
+sleep 0.5
 
-git push -u origin master
+git push --all origin master
 
-
-echo "################################################################"
-echo "###################    Git Push Done      ######################"
-echo "################################################################"
+echo "Pushed!"
